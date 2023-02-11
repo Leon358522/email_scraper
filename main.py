42b486fbdc7c8f30b90f3db1d292c690c8f82694
Leon_358522 <leonardschiller@msn.com>
2023-02-11 01:02:43 +0100

    Added logging

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import re
+import logging
 from selenium import webdriver
 
-EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[
-\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[
+WEBSITE_NAME = r'''((?<=\/\/)|(?<=\/\/www\.))[^(www)].+(?=\.)'''
+logging.basicConfig(filename='emails.log', encoding='utf-8', level=logging.INFO, format='%(message)s')
+EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[
+\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")(@|\(at\))(?:(?:[a-z0-9](?:[a-z0-9-]*[
 a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[
 0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[
 \x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+# EMAIL_REGEX1 = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[
+# \x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[
+# a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[
+# 0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[
+# \x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+# EMAIL_REGEX2 = r"""(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[
+# \x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")\(at\)(?:(?:[a-z0-9](?:[a-z0-9-]*[
+# a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[
+# 0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[
+# \x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
 EDGE_DRIVER = './edgedriver_win64/msedgedriver'
 DRIVER = webdriver.Edge(EDGE_DRIVER)
 
@@ -35,6 +48,11 @@ if __name__ == '__main__':
     for site in sites:
         mails.append(scrape_emails(site))
     for i, mails in enumerate(mails):
-        var = {f'{sites.__getitem__(i)}: {mails}'}
+        mails = list(set(mails))
+        try:
+            var = {f'CompanySiteName: {re.search(WEBSITE_NAME, sites.__getitem__(i))[0].capitalize()}: {mails}'}
+        except TypeError:
+            continue
         print(var)
+        logging.info(var)
     DRIVER.close()
